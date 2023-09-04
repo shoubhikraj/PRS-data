c137496df24936a644d1674b7514a170f3cb70ed
Shoubhik Maiti <shoubhikrajmaiti@gmail.com>
2023-09-04 20:02:20 +0100

    first commit

---
+++ ci_neb_5/reaction-7/reaction-7-ci-neb-5.py
@@ -0,0 +1,19 @@
+import autode as ade
+from autode import CINEB
+
+ade.log.logger.setLevel('INFO')
+ade.Config.ORCA.keywords.grad = ['EnGrad','PBEh-3c']
+
+rct = ade.Reactant('reaction-7-rct-orca.xyz', charge=0, mult=1)
+prod = ade.Product('reaction-7-prod-orca.xyz', charge=0, mult=1)
+
+cineb = CINEB.from_end_points(
+   rct,
+   prod,
+   num = 7,
+)
+
+cineb.calculate(ade.methods.ORCA(), n_cores=4)
+if cineb.images.contains_peak:
+   cineb.peak_species.print_xyz_file(filename='cineb-5-peak.xyz')
+print('Total number of CI-NEB iters =', cineb.images[1].iteration*5)
