5ee5b4ddfdaf243cc91c35e4514f91ea4387ed61
Shoubhik Maiti <shoubhikrajmaiti@gmail.com>
2023-09-04 20:07:03 +0100

    upload files

---
+++ i_eip/reaction-2/ts-opt/reaction-2-ieip_optts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#$ -cwd
+#$ -pe smp 8
+#$ -l s_rt=360:00:00
+#
+export ORIG=$PWD
+export SCR=$TMPDIR
+export NBOEXE=/usr/local/nbo7/bin/nbo7.i4.exe
+module load openmpi4.1.1
+cp *.xyz *.hess $SCR
+cp reaction-2-ieip_optts.inp $SCR
+cd $SCR
+/usr/local/orca_5_0_4/orca reaction-2-ieip_optts.inp > $ORIG/reaction-2-ieip_optts.out
+rm -f *.tmp
+cp -R * $ORIG
+rm *.sh.*
---
+++ i_eip/reaction-3/reaction-3-i-eip.py
@@ -0,0 +1,18 @@
+import autode as ade
+from autode.bracket.eip import IEIP
+
+ade.log.logger.setLevel('INFO')
+ade.Config.ORCA.keywords.grad = ['EnGrad','PBEh-3c']
+ade.Config.ORCA.keywords.low_sp = ['SP', 'PBEh-3c']
+
+rct = ade.Reactant('reaction-3-rct-orca.xyz', charge=-1, mult=1)
+prod = ade.Product('reaction-3-prod-orca.xyz', charge=-1, mult=1)
+
+ieip = IEIP(
+   initial_species = rct,
+   final_species = prod,
+   maxiter = 1000,
+)
+
+ieip.calculate(ade.methods.ORCA(), n_cores=4)
+print('Total number of i-EIP gradient iters =', ieip._macro_iter + 4)
